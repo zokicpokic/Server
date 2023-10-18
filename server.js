0afb862858f76710bc1e440bf529df388aa2522e
Zoran Maravic <zokicpokic@gmail.com>
2023-10-18 16:27:07 +0200

    change types

--- server.js
+++ server.js
@@ -78,16 +78,16 @@ app.post('/api/jobs', async (req, res) => {
     const pool = await sql.connect(config);
     const result = await pool
       .request()
-      .input('client_id', sql.UniqueIdentifier, newJob.client_id)
+      .input('client_id', sql.NVarChar(255), newJob.client_id)
       .input('operater_id', sql.UniqueIdentifier, newJob.operater_id)
       .input('job_date', sql.DateTime, newJob.job_date)
       .input('job_type_id', sql.UniqueIdentifier, newJob.job_type_id)
-      .input('paper_id', sql.UniqueIdentifier, newJob.paper_id)
-      .input('printer_id', sql.UniqueIdentifier, newJob.printer_id)
-      .input('envelope_id', sql.UniqueIdentifier, newJob.envelope_id)
-      .input('envelope_printer_id', sql.UniqueIdentifier, newJob.envelope_printer_id)
-      .input('envelope_ps_id', sql.UniqueIdentifier, newJob.envelope_ps_id)
-      .input('ps_machine_id', sql.UniqueIdentifier, newJob.ps_machine_id)
+      .input('paper_id', sql.NVarChar(255), newJob.paper_id)
+      .input('printer_id', sql.NVarChar(255), newJob.printer_id)
+      .input('envelope_id', sql.NVarChar(255), newJob.envelope_id)
+      .input('envelope_printer_id', sql.NVarChar(255), newJob.envelope_printer_id)
+      .input('envelope_ps_id', sql.NVarChar(255), newJob.envelope_ps_id)
+      .input('ps_machine_id', sql.NVarChar(255), newJob.ps_machine_id)
       .input('qty_lists', sql.Int, newJob.qty_lists)
       .input('qty_pages', sql.Int, newJob.qty_pages)
       .input('qty_envelope', sql.Int, newJob.qty_envelope)
@@ -151,19 +151,53 @@ app.post('/api/jobs', async (req, res) => {
 // PUT (update) an existing job by ID
 app.put('/api/jobs/:id', async (req, res) => {
   const jobId = req.params.id;
-  const updatedJob = req.body; // Assuming you send updated job data in the request body
+  const updatedJob = req.body;
+
   try {
     const pool = await sql.connect(config);
     const result = await pool
       .request()
-      .input('job_id', sql.UniqueIdentifier, jobId)
-      .input('job_type_id', sql.UniqueIdentifier, updatedJob.job_type_id)
+      .input('id', sql.UniqueIdentifier, updatedJob.id) // Added this line to use the jobId in the SQL query
       .input('client_id', sql.UniqueIdentifier, updatedJob.client_id)
-      // Add more inputs for other columns as needed
+      .input('operater_id', sql.UniqueIdentifier, updatedJob.operater_id)
+      .input('job_date', sql.DateTime, updatedJob.job_date)
+      .input('job_type_id', sql.UniqueIdentifier, updatedJob.job_type_id)
+      .input('paper_id', sql.UniqueIdentifier, updatedJob.paper_id)
+      .input('printer_id', sql.UniqueIdentifier, updatedJob.printer_id)
+      .input('envelope_id', sql.UniqueIdentifier, updatedJob.envelope_id)
+      .input('envelope_printer_id', sql.UniqueIdentifier, updatedJob.envelope_printer_id)
+      .input('envelope_ps_id', sql.UniqueIdentifier, updatedJob.envelope_ps_id)
+      .input('ps_machine_id', sql.UniqueIdentifier, updatedJob.ps_machine_id)
+      .input('qty_lists', sql.Int, updatedJob.qty_lists)
+      .input('qty_pages', sql.Int, updatedJob.qty_pages)
+      .input('qty_envelope', sql.Int, updatedJob.qty_envelope)
+      .input('qty_boxes', sql.Int, updatedJob.qty_boxes)
+      .input('start_time', sql.NVarChar(50), updatedJob.start_time)
+      .input('end_time', sql.NVarChar(50), updatedJob.end_time)
+      .input('status_id', sql.UniqueIdentifier, updatedJob.status_id)
+      .input('other', sql.NVarChar(MAX), updatedJob.other)
       .query(`
         UPDATE jobs
-        SET job_type_id = @job_type_id, client_id = @client_id, ...
-        WHERE id = @job_id;
+        SET 
+          client_id = @client_id,
+          operater_id = @operater_id,
+          job_date = @job_date,
+          job_type_id = @job_type_id,
+          paper_id = @paper_id,
+          printer_id = @printer_id,
+          envelope_id = @envelope_id,
+          envelope_printer_id = @envelope_printer_id,
+          envelope_ps_id = @envelope_ps_id,
+          ps_machine_id = @ps_machine_id,
+          qty_lists = @qty_lists,
+          qty_pages = @qty_pages,
+          qty_envelope = @qty_envelope,
+          qty_boxes = @qty_boxes,
+          start_time = @start_time,
+          end_time = @end_time,
+          status_id = @status_id,
+          other = @other
+        WHERE id = @id;
       `);
     res.json({ message: 'Job updated successfully' });
     pool.close();
